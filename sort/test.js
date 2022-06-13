6666c4bbf7acd6296fdf5f3f83e823b34d467b6e
yuankai <yuankai@yxt.com>
2022-06-13 23:27:45 +0800

    单调栈

--- sort/test.js
+++ sort/test.js
@@ -1,13 +1,21 @@
-function monotonicStack(nums) {
-  const n = nums.length
-  const stack = [],
-    res = new Array(n)
-  for (let i = n - 1; i >= 0; i--) {
-    while (stack.length && nums[i] >= stack[stack.length - 1]) {
-      stack.pop()
-    }
-    res[i] = stack.length === 0 ? -1 : stack[stack.length - 1]
-    stack.push(nums[i])
-  }
-  return res
+// function monotonicStack(nums) {
+//   const n = nums.length
+//   const stack = [],
+//     res = new Array(n)
+//   for (let i = n - 1; i >= 0; i--) {
+//     while (stack.length && nums[i] >= stack[stack.length - 1]) {
+//       stack.pop()
+//     }
+//     res[i] = stack.length === 0 ? -1 : stack[stack.length - 1]
+//     stack.push(nums[i])
+//   }
+//   return res
+// }
+
+let arr = [1, 2, 3, 4, 5]
+let n = arr.length,
+  index = 0
+while (index < 2 * n) {
+  console.log(arr[index % n])
+  index++
 }
