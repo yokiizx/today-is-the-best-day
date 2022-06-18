aca91ee907c1167687caaefa95afd556b2199e8f
yuankai <yuankai@yxt.com>
2022-06-18 11:15:35 +0800

    47

--- sort/test.js
+++ sort/test.js
@@ -20,13 +20,19 @@
 //   index++
 // }
 
-const arr = [1, 2, 3, 4, 5]
+// const arr = [1, 2, 3, 4, 5]
 
-function logReverse(arr, n) {
-  if (n < 0) return
-  console.log('前--', arr[n])
-  logReverse(arr, n - 1)
-  console.log('后====', arr[n])
+// function logReverse(arr, n) {
+//   if (n < 0) return
+//   console.log('前--', arr[n])
+//   logReverse(arr, n - 1)
+//   console.log('后====', arr[n])
+// }
+
+// logReverse(arr, arr.length - 1)
+
+function rabbit(n) {
+  if (n === 1 || n === 2) return 1
+  return rabbit(n - 1) + rabbit(n - 2)
 }
-
-logReverse(arr, arr.length - 1)
+console.log(rabbit(12))
