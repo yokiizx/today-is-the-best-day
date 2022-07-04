2a4ee4dc2d64b04b5a913a3c03b7463356bd7c63
yuankai <yuankai@yxt.com>
2022-07-04 21:41:43 +0800

    背包问题

--- sort/test.js
+++ sort/test.js
@@ -31,8 +31,72 @@
 
 // logReverse(arr, arr.length - 1)
 
-function rabbit(n) {
-  if (n === 1 || n === 2) return 1
-  return rabbit(n - 1) + rabbit(n - 2)
+// function rabbit(n) {
+//   if (n === 1 || n === 2) return 1
+//   return rabbit(n - 1) + rabbit(n - 2)
+// }
+// console.log(rabbit(12))
+
+// const map = new Map()
+// map.set('1', 1)
+// map.set('2', 2)
+// map.set('3', 3)
+
+// console.log(map.keys().next().value)
+
+class PriorityQueue {
+  // 初始化传入比较函数传入
+  constructor(data, cmp) {
+    this.data = [null].concat(data) // 注意这步操作，是为了方便计算父节点
+    this.cmp = cmp // 比较函数
+    // 这一步是为什么呢??? todo
+    for (let i = data.length >> 1; i > 0; i--) this.down(i)
+  }
+  get size() {
+    return this.data.length - 1
+  }
+  get top() {
+    return this.data[1]
+  }
+  swap(i, j) {
+    ;[this.data[i], this.data[j]] = [this.data[j], this.data[i]]
+  }
+  // 利用递归进行上浮和下沉
+  up(i) {
+    if (i === 1) return
+    const p = i >> 1
+    if (this.cmp(this.data[p], this.data[i])) {
+      this.swap(i, p)
+      this.up(p)
+    }
+  }
+  down(i) {
+    let j = i
+    const n = this.data.length
+    if (i === n - 1) return
+    const l = i << 1,
+      r = l + 1
+    if (l < n && this.cmp(this.data[i], this.data[l])) i = l
+    if (r < n && this.cmp(this.data[i], this.data[r])) i = r
+    if (j !== i) {
+      this.swap(i, j)
+      this.down(i)
+    }
+  }
+  push(val) {
+    // 把值加入末尾,然后上浮它
+    this.up(this.data.push(val) - 1)
+  }
+  pop() {
+    // 把第一个和最后一个交换, pop出需要的值,把最后一个下沉
+    this.swap(1, this.data.length - 1)
+    const res = this.data.pop()
+    this.down(1)
+    return res
+  }
 }
-console.log(rabbit(12))
+
+let arr = [3, 1, 4, 6, 2, 7, 9, 8, 0, 5]
+
+const pq = new PriorityQueue(arr, (a, b) => a - b > 0)
+console.log('pq', pq)
