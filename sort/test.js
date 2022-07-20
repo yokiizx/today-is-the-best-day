38b74110a8dfed9264f521e5930108765d00b26e
yuankai <yuankai@yxt.com>
2022-07-20 10:21:24 +0800

    13

--- sort/test.js
+++ sort/test.js
@@ -100,3 +100,14 @@ let arr = [3, 1, 4, 6, 2, 7, 9, 8, 0, 5]
 
 const pq = new PriorityQueue(arr, (a, b) => a - b > 0)
 console.log('pq', pq)
+
+const map = new Map([
+  ['I', 1],
+  ['V', 5],
+  ['X', 10],
+  ['L', 50],
+  ['C', 100],
+  ['D', 500],
+  ['M', 1000]
+])
+console.log(map)
